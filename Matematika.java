b1027d32a8f7ee9bbd7bbb60063104dbd937a840
Lidiawindikiraniaju <lidiawindi68@gmail.com>
2024-11-24 21:45:09 +0700

    Add files via upload

---
+++ Matematika.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Matematika {
+    public int Pertambahan(int a, int b){
+        return a + b;
+    }
+    public int Perkalian(int a, int b){
+        return a * b;
+    }
+    public int Modulus(int a, int b){
+        return a % b;
+    }
+
+    
+}
---
+++ MatematikaBeraksi.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        MatematikaCanggih math= new MatematikaCanggih();
+        int hasilPertambahan = math.Pertambahan(17, 5);
+        System.out.println("hasil Pertambahan:" + hasilPertambahan);
+        int hasilPerkalian = math.Perkalian(17,5);
+        System.out.println("hasil Perkalian:" + hasilPerkalian);
+        int hasilModulus = math.Modulus(19, 5);
+        System.out.println("hasil Modulus:" + hasilModulus);
+       
+        
+        
+            
+        }
+        
+    }
+    
+
